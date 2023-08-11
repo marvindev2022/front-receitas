52d500ce9ac675aaa15933d514dd1928e07fd5a1
Marcus Roza <marcusroza@pop-os.localdomain>
2023-08-10 22:43:29 -0300

    conectado com deploy backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://api-receitas-sabores-compartilhados.onrender.com/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
